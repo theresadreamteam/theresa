9df0af3bcfad1db61dfe0e0da72ac11d28610380
sam landman <sam landman>
2020-08-18 12:40:26 +0100

    added chromium and google chrome to addons

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,9 @@ require 'capybara/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+   driven_by :selenium, using: :chrome, options: { args: ["headless"] }
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
