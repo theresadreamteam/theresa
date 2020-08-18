cddab3e2a7e505d6b17b35823484d44582785c80
India Rex <face940@gmail.com>
2020-08-18 23:38:30 +0100

    removed a . in line 5 of environment.js, hoping it fixes build error for travisCI

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -4,8 +4,8 @@ class WalksController < ApplicationController
   end
 
   def all
-    @walks = Walk.all
-    walks = '[{:id=>1, :name=>"asdf", :description=>"asdf", :latitude=>"51.533136", :longitude=>"-0.129538", :postcode=>"NW11HD"}, {:id=>2, :name=>"asdf", :description=>"asdf", :latitude=>nil, :longitude=>nil, :postcode=>"asdf"}, {:id=>3, :name=>"asdf", :description=>"asdf", :latitude=>nil, :longitude=>nil, :postcode=>"sdffg"}, {:id=>4, :name=>"asdf", :description=>"asdf", :latitude=>"40.5", :longitude=>"20.5", :postcode=>"asdf"}]'
+    # @walks = Walk.all
+    # walks = '[{:id=>1, :name=>"asdf", :description=>"asdf", :latitude=>"51.533136", :longitude=>"-0.129538", :postcode=>"NW11HD"}, {:id=>2, :name=>"asdf", :description=>"asdf", :latitude=>nil, :longitude=>nil, :postcode=>"asdf"}, {:id=>3, :name=>"asdf", :description=>"asdf", :latitude=>nil, :longitude=>nil, :postcode=>"sdffg"}, {:id=>4, :name=>"asdf", :description=>"asdf", :latitude=>"40.5", :longitude=>"20.5", :postcode=>"asdf"}]'
   end
 
   def example
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,12 @@
-import { environment } from '@rails/webpacker'
+const { environment } = require('@rails/webpacker')
 
-import { ProvidePlugin } from 'webpack'
-environment.plugins.append('Provide', 
-  new ProvidePlugin({
+const webpack = require('webpack')
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
-})
-)
-export default environment
+  })
+  )
+
+module.exports = environment
