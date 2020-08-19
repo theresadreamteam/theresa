1eea257dbe0443f186cf79ec290b32b3acb07c96
India Rex <face940@gmail.com>
2020-08-19 08:31:16 +0100

    change to require from import on application.js in packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-import 'bootstrap'
+require("bootstrap").start()
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
