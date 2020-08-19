05db12ed96a7191a4e86a31016edd3ff08ca755a
sam landman <sam landman>
2020-08-19 11:13:31 +0100

    amended webpacker version

--- app/javascript/components/MapRender.js
+++ app/javascript/components/MapRender.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 class MapRender extends React.Component {
   render () {
@@ -7,6 +8,12 @@ class MapRender extends React.Component {
       <div>
         <h1>Hello {this.props.name}, </h1>
         <h3>Wow! This is a Map!</h3>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
       </div>
     );
   }
