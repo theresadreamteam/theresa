6a17ee6aadc6c7cd72867677e67dd8556f0ce082
sam landman <sam landman>
2020-08-20 10:35:24 +0100

    RSPEC TEST: Walk is valid with valid attributes

--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Walk, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "it valid with valid attributes" do
+    expect(Walk.new).to be_valid
+  end
+
+  it "it not valid with no title" do
+    #not yet implemented
+  end
+
+  it "it is not valid with no description" do
+    #not yet implemented
+  end
+
 end
