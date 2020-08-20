aef3c80c163e7d7aa533e550e9c25850f5f7e776
sam landman <sam landman>
2020-08-20 14:32:43 +0100

    created sessions and user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
   resources :walks
   root 'walks#all'
 end
