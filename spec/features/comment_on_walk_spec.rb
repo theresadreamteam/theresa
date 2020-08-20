50a0741da39edaf908969b07126a2ce958096618
India Rex <face940@gmail.com>
2020-08-20 17:07:52 +0100

    refactor first walk into module to set up db for adding comment

--- spec/features/comment_on_walk_spec.rb
+++ spec/features/comment_on_walk_spec.rb
@@ -1,16 +1,11 @@
 require 'rails_helper'
+require 'helpers/first_walk_helper'
 
 RSpec.describe 'Theresa', type: :system do
   describe 'comment on a walk' do
+    include FirstWalkHelper
     it 'shows your comment on the walk page' do
-      visit ('/')
-      click_on 'add walk'
-      fill_in 'walk_title', with: 'Canalside walk'
-      fill_in 'walk_description', with: 'A beautiful long walk by the water'
-      fill_in 'walk_coordinates_start', with: '51.544514, -0.023015'
-      fill_in 'walk_coordinates_end', with: '51.544514, -0.023015'
-      fill_in 'walk_distance', with: '5'
-      click_on 'add this walk'
+      add_first_walk
       fill_in 'comment_body', with: 'This one really is lovely'
       fill_in 'comment_commenter', with: 'Warbling Walker'
       click_on 'add comment'
