be319f4c8e2f83ded431a846520ecd6a9e95955f
India Rex <face940@gmail.com>
2020-08-20 17:15:02 +0100

    unpend tests that had x next to them in rspec

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Theresa', type: :system do
-  xdescribe 'index page' do
+  describe 'index page' do
     it 'shows the content "Theresa" on the index page' do
       visit ('/')
       expect(page).to have_content 'Theresa'
