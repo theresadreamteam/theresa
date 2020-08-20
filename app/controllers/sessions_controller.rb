249e3621826c1179aa9cfe4772f31267cd81fc85
sam landman <sam landman>
2020-08-20 14:52:36 +0100

    created functionality for user signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
    if @user && @user.authenticate(params[:password])
-      sessions[:user_id] = @user.id
-      redirect_to '/welcome'
+      session[:user_id] = @user.id
+      redirect_to '/'
    else
       redirect_to '/login'
    end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     # session[:user_id] = @user.id
-    redirect_to '/signin'
+    redirect_to '/login'
   end
 
   private
