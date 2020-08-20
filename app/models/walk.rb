77cbdf8ee1dad805164dbe850153d94393ee8f1d
sam landman <sam landman>
2020-08-20 10:49:51 +0100

    RSPEC TEST: Walk is not valid without title, description or start coordinate

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,5 +1,8 @@
 class Walk < ApplicationRecord
   include Rails.application.routes.url_helpers
   has_one_attached :photo
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :coordinates_start
 
 end
--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Walk, type: :model do
-  it "it valid with valid attributes" do
-    expect(Walk.new).to be_valid
+  subject {
+    described_class.new(title: "First Walk",
+                        description: "Beautiful Walk",
+                        coordinates_start: "1.000 , 2.000")
+  }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
   end
 
-  it "it not valid with no title" do
-    #not yet implemented
+  it "is not valid with no title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
   end
 
-  it "it is not valid with no description" do
-    #not yet implemented
+  it "is is not valid with no description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with no start_coordinate" do
+    subject.coordinates_start = nil
+    expect(subject).to_not be_valid
   end
 
 end
