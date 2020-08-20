c37474d365915bdfc8ef09cffb097afeff871ce5
sam landman <sam landman>
2020-08-20 14:48:47 +0100

    created methods, and added bcrypt to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
+  resources :users, only: [:new, :create]
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   get 'welcome', to: 'sessions#welcome'
+
   resources :walks
+  
   root 'walks#all'
 end
