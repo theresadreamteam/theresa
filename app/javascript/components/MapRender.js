06ca5b046d94c60591ea53486c55034ba22c24aa
India Rex <face940@gmail.com>
2020-08-21 16:15:52 +0100

    part way to making the maps work, but need to update branch with new functionality because db is different now.

--- app/javascript/components/MapRender.js
+++ app/javascript/components/MapRender.js
@@ -2,27 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 require('dotenv').config();
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-const { REACT_APP_GOOGLE_MAP_KEY } = process.env;
-
-console.log('Enviroment Key');
-console.log(process.env.NODE_ENV);
-console.log('Google Map Key'+REACT_APP_GOOGLE_MAP_KEY)
 
 class MapRender extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
+      // this returns all the walks in the db
       walks: this.props.walks
     }
   }
 
   displayMarkers = () => {
-    return this.state.walks.map((walk, index) => {
+    return this.state.walks.map((walk, index) => { 
       return <Marker key={index} id={index} position={{
-       lat: walk.latitude,
-       lng: walk.longitude
+       lat: parseFloat(walk.latitude),
+       lng: parseFloat(walk.longitude)
      }}
      onClick={() => console.log("You clicked me! index: "+index)} />
     })
@@ -33,6 +28,7 @@ class MapRender extends React.Component {
       <div>
         <h1>Hello {this.props.name}, </h1>
         <h3>Wow! This is a Map!</h3>
+        {console.log(this.props.walks)}
         <Map
           google={this.props.google}
           zoom={8}
@@ -42,7 +38,7 @@ class MapRender extends React.Component {
 
           {this.displayMarkers()}
           <Marker position={{ lat: this.props.latitude, lng: this.props.longitude}} />
-
+          
         </Map>
       </div>
     );
@@ -51,11 +47,11 @@ class MapRender extends React.Component {
 
   MapRender.propTypes = {
     name: PropTypes.string,
-    latitude: PropTypes.integer,
-    longitude: PropTypes.integer,
+    latitude: PropTypes.string,
+    longitude: PropTypes.string,
     walks: PropTypes.array
   };
 
   export default GoogleApiWrapper ({
-      apiKey: googlemapkey
+    apiKey: '-zBb2TWdxqg'
   })(MapRender);
