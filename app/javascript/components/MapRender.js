bba473617d77a423dac6ee0ed448a29de49d0b4b
sam landman <sam landman>
2020-08-25 13:41:50 +0100

    added functionality for zoom in and zoom out

--- app/javascript/components/MapRender.js
+++ app/javascript/components/MapRender.js
@@ -14,6 +14,7 @@ class MapRender extends React.Component {
       mapState : {
         latitude: this.props.latitude,
         longitude: this.props.longitude,
+        zoom: 8,
         circle: {
           radius: 50000,
           strokeColor: "#FF0000",
@@ -38,6 +39,18 @@ class MapRender extends React.Component {
     this.setState({tempState});
   }
 
+  zoomout = () => {
+    let tempState = this.state;
+    tempState.mapState.zoom -= 1;
+    this.setState({tempState})
+  }
+
+  zoomin = () => {
+    let tempState = this.state;
+    tempState.mapState.zoom += 1;
+    this.setState({tempState})
+  }
+
   setLocation = () => {
     let tempState = this.state;
     tempState.mapState.longitude = this.props.longitude;
@@ -45,13 +58,48 @@ class MapRender extends React.Component {
     this.setState({tempState});
   }
 
+  calculateDistance(pointA, pointB) {
+
+    const lat1 = pointA.latitude;
+    const lon1 = pointA.longitude;
+  
+    const lat2 = pointB.latitude;
+    const lon2 = pointB.longitude;
+  
+    const R = 6371e3; // earth radius in meters
+    const φ1 = lat1 * (Math.PI / 180);
+    const φ2 = lat2 * (Math.PI / 180);
+    const Δφ = (lat2 - lat1) * (Math.PI / 180);
+    const Δλ = (lon2 - lon1) * (Math.PI / 180);
+  
+    const a = (Math.sin(Δφ / 2) * Math.sin(Δφ / 2)) +
+              ((Math.cos(φ1) * Math.cos(φ2)) * (Math.sin(Δλ / 2) * Math.sin(Δλ / 2)));
+    
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    const distance = R * c;
+    return distance; // in meters
+  }
+
   displayMarkers = () => {
+    var latLngB = { 
+      latitude: this.state.mapState.latitude,
+      longitude: this.state.mapState.longitude
+    }
+
     return this.state.walks.map((walk, index) => {
+      var latLngA = {
+        latitude: walk.latitude, 
+        longitude: walk.longitude
+      }
+      if (this.calculateDistance(latLngA, latLngB) < this.state.mapState.circle.radius) {
       return <Marker key={index} id={index} position={{
        lat: walk.latitude,
        lng: walk.longitude
      }}
+     
      onClick={() => this.displayPlace(walk)} />
+      }
     })
   }
 
@@ -61,6 +109,29 @@ class MapRender extends React.Component {
     tempState.mapState.latitude = walk.latitude;
     tempState.mapState.longitude = walk.longitude;
     this.setState({tempState});
+  }
+
+  displayWalks() {
+    
+    var latLngB = { 
+      latitude: this.state.mapState.latitude,
+      longitude: this.state.mapState.longitude
+    }
+
+    return this.state.walks.map((walk, index) => {
+      var latLngA = {
+        latitude: walk.latitude, 
+        longitude: walk.longitude
+      }
+      if (this.calculateDistance(latLngA, latLngB) < this.state.mapState.circle.radius) {
+      console.log("Show walk"+index)
+      return (
+        <div id={"walk"+index}>
+        <p> {walk.title}</p>
+        </div>
+        );
+      }
+    })
   }
 
   render () {
@@ -73,6 +144,8 @@ class MapRender extends React.Component {
             <ButtonGroup aria-label="Set Radius">
               <Button onClick={() => this.incrementRadius()}>Increase Drive Time: 15 mins</Button>
               <Button onClick={() => this.decreaseRadius()}>Decrease Drive Time: 15 mins</Button>
+              <Button onClick={() => this.zoomin()}>Zoom In</Button>
+              <Button onClick={() => this.zoomout()}>Zoom Out</Button>
             </ButtonGroup>
             <br></br>
             <label>Time in Car </label>
@@ -84,13 +157,14 @@ class MapRender extends React.Component {
               />
               <ButtonGroup aria-label="Reset Location">
               <Button onClick={() => this.setLocation()}>Reset Location</Button>
+              <Button onClick={() => this.setLocation()}>Display All Walks</Button>
             </ButtonGroup>
           </Row>
           <br></br>
           <Row>
           <Map
             google={this.props.google}
-            zoom={8}
+            zoom={this.state.mapState.zoom}
             class = "mapStyles"
             initialCenter={{ lat: 51.51736, lng: -0.073328}}
             >
@@ -109,6 +183,9 @@ class MapRender extends React.Component {
                   fillOpacity={this.state.mapState.circle.fillOpacity}
                 />  
           </Map>
+          </Row>
+          <Row>
+            {this.displayWalks()}
           </Row>
         </Container>
       </React.Fragment>
