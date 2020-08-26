a65420a5f842e60d803f8176e8dc7add12ab349b
India Rex <face940@gmail.com>
2020-08-26 14:38:05 +0100

    part done test

---
+++ spec/controllers/comment_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'comment', type: :controller do
+  describe '' do
+  
+    
+  
+  end
+end
+
+
+require 'rails_helper'
+require 'helpers/first_walk_helper'
+
+RSpec.describe 'Theresa', type: :system do
+  describe 'comment on a walk' do
+    include FirstWalkHelper
+    it 'shows your comment on the walk page' do
+      sign_up
+      sign_in
+      add_first_walk
+      fill_in 'comment_body', with: 'This one really is lovely'
+      click_on 'add comment'
+      expect(page).to have_content 'test says'
+      expect(page).to have_content 'This one really is lovely'
+    end
+  end
+  
+end
