404498fa29662f61f19609c03055bbd4c8af2d0a
Iryna Audzei <irynaaudzei@macbook-pro.mynet>
2020-08-26 16:51:27 +0100

    Added one Jest test

--- app/javascript/WalkWeather.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import WalkWeather from './WalkWeather';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<WalkWeather />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,3 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+test('renders weather for the walk', () => {
+  const walkWeather = new WalkWeather();
+  expect(walkWeather).not.toBeNull;
+});
